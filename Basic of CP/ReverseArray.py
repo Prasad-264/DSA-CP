b2186eff065616338814290b349b9294755ce815
Prasad Kandekar <prasadkandekar555@gmail.com>
2021-10-28 10:21:31 +0530

    another method

--- Basic of CP/ReverseArray.py
+++ Basic of CP/ReverseArray.py
@@ -7,3 +7,20 @@ A = [1, 2, 3, 4, 5, 6]
 print(A)
 print("Reversed list is")
 reverseList(A)
+
+
+# Recursive python program to reverse an array
+ 
+# Function to reverse A[] from start to end
+def reverseList(A, start, end):
+    if start >= end:
+        return
+    A[start], A[end] = A[end], A[start]
+    reverseList(A, start+1, end-1)
+ 
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print(A)
+reverseList(A, 0, 5)
+print("Reversed list is")
+print(A)
