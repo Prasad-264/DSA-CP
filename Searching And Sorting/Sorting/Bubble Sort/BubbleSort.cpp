2f03478c8112eef9ef109d8c0510d24cd7c9a48a
Prasad Kandekar <prasadkandekar555@gmail.com>
2021-12-11 19:33:32 +0530

    bubble sort update

--- Searching And Sorting/Sorting/Bubble Sort/BubbleSort.cpp
+++ Searching And Sorting/Sorting/Bubble Sort/BubbleSort.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++)    
+
+    bool swapped = false;
+
+    for (int i = 0; i < n-1; i++) {    
      
-    // Last i elements are already in place
-    for (int j = 0; j < n-i-1; j++)
-        if (arr[j] > arr[j+1])
-            swap(arr[j], arr[j+1]);
+        // Last i elements are already in place
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+        }
+
+        if(swapped == false)
+            break ;       
+    }
 }
  
 /* Function to print an array */
@@ -27,3 +36,7 @@ int main() {
     printArray(arr, n);
     return 0;
 }
+
+// TC is O(N*N)
+// Best case time complexity is O(N)
+// Worst case time complexity is O(N*N)
