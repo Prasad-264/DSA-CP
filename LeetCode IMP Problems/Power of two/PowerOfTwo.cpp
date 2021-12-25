fed6e9a760b1cbed93670facbc16ccd1e0ab68e7
Prasad Kandekar <prasadkandekar555@gmail.com>
2021-12-25 12:08:06 +0530

    Longest Substring

--- LeetCode IMP Problems/Power of two/PowerOfTwo.cpp
+++ LeetCode IMP Problems/Power of two/PowerOfTwo.cpp
@@ -1,3 +1,10 @@
+/*
+bit representation of 7  -> 0111
+bit representation of 8  -> 1000
+bitwise AND of 7 and 8 -> 0000
+we are gonna use this property for for all numbers which are powers of two
+*/
+
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
