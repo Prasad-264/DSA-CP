cf23e9b804889124eb62af53fe7fd7b0c3944033
Prasad Kandekar <prasadkandekar555@gmail.com>
2022-01-13 16:38:17 +0530

    Duplicate in array

---
+++ Amazon Interview Problems/Array/Find duplicates in an array/FIndDuplicate.cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        int hash[n]={0};
+        for(int i=0;i<n;i++) {
+            hash[arr[i]]++;
+        }
+        vector<int> res;
+        for(int i=0;i<n;i++) {
+            if(hash[i]>1)
+                res.push_back(i);
+        }
+        if(res.size()==0) {
+            res.push_back(-1);
+            return res;
+        }
+        return res;
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
