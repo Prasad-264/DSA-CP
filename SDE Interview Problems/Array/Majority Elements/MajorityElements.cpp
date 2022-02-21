5e317e4a0615ff8ab37a5880e158082fdcf039a9
Prasad Kandekar <prasadkandekar555@gmail.com>
2022-02-21 09:27:43 +0530

    Update Majority element

--- SDE Interview Problems/Array/Majority Elements/MajorityElements.cpp
+++ SDE Interview Problems/Array/Majority Elements/MajorityElements.cpp
@@ -1,3 +1,25 @@
+/*
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+
+
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+*/
+
 // { Driver Code Starts
 //Initial template for C++
 
@@ -8,6 +30,7 @@ using namespace std;
  // } Driver Code Ends
 //User function template for C++
 
+/*
 class Solution{
 public:
      // Function to find majority element in the array
@@ -24,6 +47,27 @@ public:
         return -1;
     }
 };
+
+Time Complexity : O(N)
+Space Complexity : O(N)
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int counter = 0, majority;
+        for (int num : nums) {
+            if (!counter) {
+                majority = num;
+            }
+            counter += num == majority ? 1 : -1;
+        }
+        return majority;
+    }
+};
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
 
 // { Driver Code Starts.
 
