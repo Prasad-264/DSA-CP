449b31e237d22952e0702085930cecc6f3e85292
Prasad Kandekar <prasadkandekar555@gmail.com>
2022-02-22 18:48:37 +0530

    01 Matrix

---
+++ SDE Interview Problems/2D Array/01 Matrix/updateMatrix.cpp
@@ -0,0 +1,89 @@
+/*
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two adjacent cells is 1.
+
+Example 1:
+
+Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+Output: [[0,0,0],[0,1,0],[0,0,0]]
+
+Example 2:
+
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+Output: [[0,0,0],[0,1,0],[1,2,1]]
+
+*/
+
+// ! Brute Force (TLE) 
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        if (rows == 0)
+            return matrix;
+        int cols = matrix[0].size();
+        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    dist[i][j] = 0;
+                } else {
+                    for (int k = 0; k < rows; k++) {
+                        for (int l = 0; l < cols; l++) {
+                            if (matrix[k][l] == 0) {
+                                int dist_01 = abs(k - i) + abs(l - j);
+                                dist[i][j] = min(dist[i][j], abs(k - i) + abs(l - j));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+// Time complexity: O((r.c)^2)
+
+// ! Optimal (DP) 
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        if (rows == 0) 
+            return matrix;
+        int cols = matrix[0].size();
+        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX - 100000));
+
+        //First pass: check for left and top
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    dist[i][j] = 0;
+                } else {
+                    if (i > 0)
+                        dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
+                    if (j > 0)
+                        dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
+                }
+            }
+        }
+
+        //Second pass: check for bottom and right
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 0; j--) {
+                if (i < rows - 1)
+                    dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
+                if (j < cols - 1)
+                    dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
+            }
+        }
+        return dist;
+    }
+};
+
+// Time complexity: O(r⋅c) :)
+// Problem link : https://leetcode.com/problems/01-matrix/
